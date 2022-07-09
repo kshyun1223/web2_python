8a90a9677d0f3ca66602c0a4bc58a617386e2eae
Seunghyun Kim <kshyun.dev@gmail.com>
2022-07-09 20:13:05 +0900

    편집

--- index.py
+++ index.py
@@ -3,7 +3,10 @@ print("Content-Type: text/html")
 print()
 import cgi
 form = cgi.FieldStorage()
-pageId = form.getvalue("id")
+if 'id' in form:
+    pageId = form.getvalue("id")
+else:
+    pageId = 'Welcome'
 print('''<!doctype html>
 <html>
 <head>
@@ -18,7 +21,7 @@ print('''<!doctype html>
     <li><a href="index.py?id=JavaScript">JavaScript</a></li>
   </ol>
   <h2>{title}</h2>
-  <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet. English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland. The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
+  <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
   </p>
 </body>
 </html>
